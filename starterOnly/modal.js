45b87cc857a8581fe76dab14ba7ef8371ba185c2
Alexandre LOPIN <alex.lopin@laposte.net>
2023-01-04 18:43:35 +0100

    first commit

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -9,7 +9,9 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const content = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const closeModalBtn = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -17,7 +19,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  content.classList.add("show");
+  modalbg.classList.add("show");
 }
 
+// launch close event
+closeModalBtn.addEventListener("click", closeModal);
 
+// launch close form
+function closeModal() {
+  content.classList.remove("show");
+  modalbg.classList.remove("show");
+}
